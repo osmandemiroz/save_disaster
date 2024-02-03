4ffb4f7dcbe3befdd794b7f12b1f9f2176038a53
Osman Demiröz <osmanndemirozz@gmail.com>
2024-02-03 11:54:43 +0300

    environment manager added

---
+++ lib/product/config/env.dart
@@ -0,0 +1,35 @@
+import 'package:envied/envied.dart';
+
+part 'env.g.dart';
+
+@Envied(
+  path: 'assets/env/.env.dev',
+  obfuscate: true,
+)
+
+///Environment variables
+abstract class Env {
+  @EnviedField(varName: 'FIREBASE_WEB_API_KEY')
+
+  ///firebase web api key
+  static final String firebaseWebApiKey = _Env.firebaseWebApiKey;
+  @EnviedField(varName: 'FIREBASE_ANDROID_API_KEY')
+
+  ///firebase android api key
+  static final String firebaseAndroidApiKey = _Env.firebaseAndroidApiKey;
+
+  @EnviedField(varName: 'FIREBASE_IOS_API_KEY')
+
+  ///firebase ios api key
+  static final String firebaseIOSApiKey = _Env.firebaseIOSApiKey;
+
+  @EnviedField(varName: 'FIREBASE_MACOS_API_KEY')
+
+  ///firebase macos api key
+  static final String firebaseMacOSApiKey = _Env.firebaseMacOSApiKey;
+
+  @EnviedField(varName: 'NEWS_API_KEY')
+
+  ///news api key
+  static final String newsApiKey = _Env.newsApiKey;
+}
